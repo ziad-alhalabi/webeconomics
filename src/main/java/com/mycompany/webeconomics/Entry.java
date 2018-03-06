e282fb22cf61c14136f64daca77d7acd472ef528
Shahroz Ahmed <shahroz.ahmed@stfc.ac.uk>
2018-03-06 16:05:33 +0000

    Normalisation of data from file

--- src/main/java/com/mycompany/webeconomics/Entry.java
+++ src/main/java/com/mycompany/webeconomics/Entry.java
@@ -11,56 +11,56 @@ package com.mycompany.webeconomics;
  */
 public class Entry {
 
-    public String weekday;
-    public String hour;
+    public int weekday;
+    public int hour;
     public String bidId;
     public String userId;
     public String useragent;
     public String IP;
-    public String region;
-    public String city;
-    public String adexchange;
+    public int regionCode;
+    public int cityCode;
+    public int adexchange;
     public String domain;
     public String url;
     public String urlId;
     public String slotId;
-    public String slotWidth;
-    public String slotHeight;
+    public int slotWidth;
+    public int slotHeight;
     public String slotVisibility;
     public String slotFormat;
-    public String slotPrice;
+    public int slotPrice;
     public String creative;
     public String keypage;
-    public String advertiser;
+    public int advertiser;
     public String userTag;
 
     public Entry(String[] nextRecord) {
-        weekday = nextRecord[0];
-        hour = nextRecord[1];
+        weekday = Integer.valueOf(nextRecord[0]);
+        hour = Integer.valueOf(nextRecord[1]);
         bidId = nextRecord[2];
         userId = nextRecord[3];
         useragent = nextRecord[4];
         IP = nextRecord[5];
-        region = nextRecord[6];
-        city = nextRecord[7];
-        adexchange = nextRecord[8];
+        regionCode = Integer.valueOf(nextRecord[6]);
+        cityCode = Integer.valueOf(nextRecord[7]);
+        adexchange = Integer.valueOf(nextRecord[8]);
         domain = nextRecord[9];
         url = nextRecord[10];
         urlId = nextRecord[11];
         slotId = nextRecord[12];
-        slotWidth = nextRecord[13];
-        slotHeight = nextRecord[14];
+        slotWidth = Integer.valueOf(nextRecord[13]);
+        slotHeight = Integer.valueOf(nextRecord[14]);
         slotVisibility = nextRecord[15];
         slotFormat = nextRecord[16];
-        slotPrice = nextRecord[17];
+        slotPrice = Integer.valueOf(nextRecord[17]);
         creative = nextRecord[18];
         keypage = nextRecord[19];
-        advertiser = nextRecord[20];
+        advertiser = Integer.valueOf(nextRecord[20]);
         userTag = nextRecord[21];
     }
 
     @Override
     public String toString() {
-        return "Entry{" + "weekday=" + weekday + ", hour=" + hour + ", bidId=" + bidId + ", userId=" + userId + ", useragent=" + useragent + ", IP=" + IP + ", region=" + region + ", city=" + city + ", adexchange=" + adexchange + ", domain=" + domain + ", url=" + url + ", urlId=" + urlId + ", slotId=" + slotId + ", slotWidth=" + slotWidth + ", slotHeight=" + slotHeight + ", slotVisibility=" + slotVisibility + ", slotFormat=" + slotFormat + ", slotPrice=" + slotPrice + ", creative=" + creative + ", keypage=" + keypage + ", advertiser=" + advertiser + ", userTag=" + userTag + '}';
+        return "Entry{" + "weekday=" + weekday + ", hour=" + hour + ", bidId=" + bidId + ", userId=" + userId + ", useragent=" + useragent + ", IP=" + IP + ", region=" + regionCode + ", city=" + cityCode + ", adexchange=" + adexchange + ", domain=" + domain + ", url=" + url + ", urlId=" + urlId + ", slotId=" + slotId + ", slotWidth=" + slotWidth + ", slotHeight=" + slotHeight + ", slotVisibility=" + slotVisibility + ", slotFormat=" + slotFormat + ", slotPrice=" + slotPrice + ", creative=" + creative + ", keypage=" + keypage + ", advertiser=" + advertiser + ", userTag=" + userTag + '}';
     }
 }
